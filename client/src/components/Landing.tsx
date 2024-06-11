03d67080a6691748a49706b144b363e7a919ebbb
123Satyajeet123 <satyajeetdas225@gmail.com>
2024-06-12 01:00:53 +0530

    basic mapping

---
+++ client/src/components/Landing.tsx
@@ -0,0 +1,18 @@
+import  { useState } from 'react';
+import {Link} from 'react-router-dom';
+
+
+const Landing = () => {
+
+    const [name, setName] = useState('');
+
+    return (
+        <div>
+            <h1>Landing</h1>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            <Link to={`/room/?name=${name}`} >Submit</Link>
+        </div>
+    );
+    }
+
+export default Landing;
